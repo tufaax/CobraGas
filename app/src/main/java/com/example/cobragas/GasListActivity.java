97dc1ec5ff4d8c50946e7c25fbfd534979defd3c
Abdulhakim Darman <abdul10hakim@gmail.com>
2020-03-20 14:54:18 -0400

    Pushing project out

---
+++ app/src/main/java/com/example/cobragas/GasListActivity.java
@@ -0,0 +1,4 @@
+package com.example.cobragas;
+
+public class GasListActivity {
+}
---
+++ app/src/main/java/com/example/cobragas/GasMapActivity.java
@@ -0,0 +1,14 @@
+package com.example.cobragas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GasMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
