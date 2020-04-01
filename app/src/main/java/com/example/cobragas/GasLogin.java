6981741fef74a9c4a0ff841217bbe7557d474f84
phat <ngo>
2020-04-01 14:25:15 -0400

    Done lets goooo

--- app/src/main/java/com/example/cobragas/GasLogin.java
+++ app/src/main/java/com/example/cobragas/GasLogin.java
@@ -1,13 +1,94 @@
 package com.example.cobragas;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GasLogin extends AppCompatActivity {
+    private VideoView videoBG;
+    MediaPlayer mMediaPlayer;
+    int mCurrentVideoPosition;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.main2);
+        initLogin();
+
+        // Hook up the VideoView to our UI.
+        videoBG = (VideoView) findViewById(R.id.videoView);
+
+        // Build your video Uri
+        Uri uri = Uri.parse("android.resource://" // First start with this,
+                + getPackageName() // then retrieve your package name,
+                + "/" // add a slash,
+                +R.raw.test); // and then finally add your video resource. Make sure it is stored
+        // in the raw folder.
+
+        // Set the new Uri to our VideoView
+        videoBG.setVideoURI(uri);
+        // Start the VideoView
+        videoBG.start();
+
+        // Set an OnPreparedListener for our VideoView. For more information about VideoViews,
+        // check out the Android Docs: https://developer.android.com/reference/android/widget/VideoView.html
+        videoBG.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mMediaPlayer = mediaPlayer;
+                // We want our video to play over and over so we set looping to true.
+                mMediaPlayer.setLooping(true);
+                // We then seek to the current posistion if it has been set and play the video.
+                if (mCurrentVideoPosition != 0) {
+                    mMediaPlayer.seekTo(mCurrentVideoPosition);
+                    mMediaPlayer.start();
+                }
+            }
+        });
+
     }
+    private void initLogin() {
+        Button ibList = (Button) findViewById(R.id.btLogInButton);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GasLogin.this, GasActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Capture the current video position and pause the video.
+        mCurrentVideoPosition = mMediaPlayer.getCurrentPosition();
+        videoBG.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Restart the video when resuming the Activity
+        videoBG.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // When the Activity is destroyed, release our MediaPlayer and set it to null.
+        mMediaPlayer.release();
+        mMediaPlayer = null;
+    }
+
 }
